095bc9fcd3c4bd19da19f8e1be340769054da0e9
Axe-- <shikharsingh@hotmail.com>
2022-05-21 17:39:50 -0700

    conceptnet multi-proc added!

--- utils.py
+++ utils.py
@@ -1,4 +1,17 @@
+import time
 import json
+import torch
+from torch.distributions import Categorical
+
+
+def compute_entropy(freqs: list):
+    """The entropy is computed with natural logarithm (`e`)"""
+    # tensor
+    freqs = torch.tensor(freqs)
+    # entropy
+    entropy = Categorical(freqs).entropy()
+
+    return entropy
 
 
 # ------------------------------------------------
@@ -11,3 +24,13 @@ def read_json(path):
 def save_json(data, path: str):
     with open(path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+class Timer:
+    def __init__(self, tag=''):
+        self.tag = tag
+        self.start = time.time()
+
+    def end(self):
+        time_taken = time.time() - self.start
+        print('{} completed in {:.2f} secs'.format(self.tag, time_taken))
